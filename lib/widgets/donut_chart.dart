500abaf9a62722f755748c3ae607475281b6d49a
akito.hagio <hagio.akito@moneyforward.co.jp>
2023-09-30 21:09:23 +0900

    Add donut chart

---
+++ lib/widgets/donut_chart.dart
@@ -0,0 +1,69 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class AppDonutChart extends StatelessWidget {
+  const AppDonutChart({super.key, required this.amount, required this.value, required this.color});
+
+  final int amount;
+  final int value;
+  final Color color;
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      child: Stack(
+        children: [
+          Positioned(
+            child: Container(
+              color: Colors.black,
+            ),
+          ),
+          Center(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.baseline,
+              textBaseline: TextBaseline.alphabetic,
+              children: [
+                Text(
+                  "$value",
+                  style: const TextStyle(
+                      fontSize: 60,
+                      color: Colors.white,
+                  ),
+                ),
+                const Text(
+                  "%",
+                  style: TextStyle(
+                    fontSize: 30,
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          PieChart(
+            PieChartData(
+              startDegreeOffset: 270,
+              sections: [
+                PieChartSectionData(
+                  color: Colors.blue, // TODO: color map
+                  value: value.toDouble(),
+                  title: "",
+                  radius: 30,
+                ),
+                PieChartSectionData(
+                  color: Colors.white,
+                  value: (amount - value).toDouble(),
+                  title: "",
+                  radius: 30,
+                ),
+              ],
+              sectionsSpace: 0,
+              centerSpaceRadius: 120,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
