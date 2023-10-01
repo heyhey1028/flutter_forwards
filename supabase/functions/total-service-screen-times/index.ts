78d64607517ee54a1090c779c43bab25636bdeb9
4geru <westhouse51@gmail.com>
2023-10-01 11:03:19 +0900

    add functions

---
+++ supabase/functions/total-service-screen-times/index.ts
@@ -0,0 +1,28 @@
+// Follow this setup guide to integrate the Deno language server with your editor:
+// https://deno.land/manual/getting_started/setup_your_environment
+// This enables autocomplete, go to definition, etc.
+
+import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+
+console.log("Hello from Functions!")
+
+serve(async (req) => {
+  const data = {
+    total_service_screen_times: 1800,
+    monthly_target: 1800,
+    icon_path: 'https://ca.slack-edge.com/T02D9RVN1-U9ZR2GHQC-0998325a042e-512'
+  }
+  const u=new URL(req.url)
+  u.searchParams.get('user_id')
+
+  return new Response(
+    JSON.stringify(data),
+    { headers: { "Content-Type": "application/json" } },
+  )
+})
+
+// To invoke:
+// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
+//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
+//   --header 'Content-Type: application/json' \
+//   --data '{"name":"Functions"}'
