9eaf9b80b77b86f4377119d4ef4255d00858a700
4geru <westhouse51@gmail.com>
2023-10-01 11:43:44 +0900

    implement total service screen time

--- supabase/functions/total-service-screen-times/index.ts
+++ supabase/functions/total-service-screen-times/index.ts
@@ -3,26 +3,45 @@
 // This enables autocomplete, go to definition, etc.
 
 import { serve } from "https://deno.land/std@0.168.0/http/server.ts"
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
+import { corsHeaders } from '../_shared/cors.ts'
 
 console.log("Hello from Functions!")
 
-serve(async (req) => {
-  const data = {
-    total_service_screen_times: 1800,
-    monthly_target: 1800,
-    icon_path: 'https://ca.slack-edge.com/T02D9RVN1-U9ZR2GHQC-0998325a042e-512'
+serve(async (req: Request) => {
+  // This is needed if you're planning to invoke your function from a browser.
+  if (req.method === 'OPTIONS') {
+    return new Response('ok', { headers: corsHeaders })
   }
-  const u=new URL(req.url)
-  u.searchParams.get('user_id')
 
-  return new Response(
-    JSON.stringify(data),
-    { headers: { "Content-Type": "application/json" } },
-  )
+  try {
+    const u=new URL(req.url)
+    // Create a Supabase client with the Auth context of the logged in user.
+    const supabaseClient = createClient(
+      // Supabase API URL - env var exported by default.
+      Deno.env.get('SUPABASE_URL') ?? '',
+      // Supabase API ANON KEY - env var exported by default.
+      Deno.env.get('SUPABASE_ANON_KEY') ?? '',
+      // Create client with Auth context of the user that called the function.
+      // This way your row-level-security (RLS) policies are applied.
+      { global: { headers: { Authorization: req.headers.get('Authorization')! } } }
+    )
+    // And we can run queries in the context of our authenticated user
+    const { data, error } = await supabaseClient.from('total_user_screen_times')
+      .select('*')
+      .eq('user_id', u.searchParams.get('user_id'))
+
+      // .eq('user_id', req.body?.getReader('user_id'))
+    if (error) throw error
+
+    return new Response(JSON.stringify({ data }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 200,
+    })
+  } catch (error) {
+    return new Response(JSON.stringify({ error: error.message }), {
+      headers: { ...corsHeaders, 'Content-Type': 'application/json' },
+      status: 400,
+    })
+  }
 })
-
-// To invoke:
-// curl -i --location --request POST 'http://localhost:54321/functions/v1/' \
-//   --header 'Authorization: Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0' \
-//   --header 'Content-Type: application/json' \
-//   --data '{"name":"Functions"}'
